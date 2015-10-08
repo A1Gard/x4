736f1fb77da9dcdd6c927450aec43a8980740b3e
A1Gard <infro@pxt.ir>
2015-10-08 11:10:02 +0330

    add Registry registry functions

--- x4.pas
+++ x4.pas
@@ -1,16 +1,17 @@
 unit x4;
-(**
- * name : x4
- * relase: 30 sep 2015 .
- * requerment: Delphi XE2 .
- * update : none .
- * website : www.4xmen.ir
- **)
+(* *
+  * name : x4
+  * relase: 30 sep 2015 .
+  * requerment: Delphi XE2 .
+  * update : 9 Oct 2015 .
+  * website : www.4xmen.ir
+  * *)
 
 interface
 
 uses System.SysUtils, System.Classes, Vcl.Forms, IdCoder, IdCoderMIME, IdGlobal,
-  Data.DB, Data.Win.ADODB, Data.DBXMSSQL, Data.SqlExpr,Winapi.msxml,Winapi.Windows;
+  Data.DB, Data.Win.ADODB, Data.DBXMSSQL, Data.SqlExpr, Winapi.msxml,
+  Winapi.Windows, System.Win.Registry;
 
 type
 
@@ -27,7 +28,7 @@ type
   private
   var
     Head: TAssoc;
-    xml : IXMLDOMDocument;
+    xml: IXMLDOMDocument;
     function FindKeyOffset(cHead: PAssoc; Key: ShortString): PAssoc;
   public
     constructor Create();
@@ -36,8 +37,8 @@ type
     procedure vDel(Key: ShortString);
     procedure vSetChild(parent, Key: string; Value: Variant);
     function toXML(cHead: PAssoc): WideString;
-    procedure fromXML(xFile:widestring);
-    
+    procedure fromXML(xFile: WideString);
+
   published
 
   end;
@@ -47,9 +48,10 @@ function GetOSType(): string; // Requires XE2 or newer.
 function GetOSVerstion(): string; // Requires XE2 or newer.
 function GetOSName(): string; // Requires XE2 or newer.
 function GetOSDetail(): string; // Requires XE2 or newer.
-procedure FatalError(Title,Text:string;Terminate:Boolean);
+procedure FatalError(Title, Text: string; Terminate: Boolean);
 
-function Explode(Delimiter: Char; Str: string): TStrings; // Requires D2006 or newer.
+function Explode(Delimiter: Char; Str: string): TStrings;
+// Requires D2006 or newer.
 function Implode(Str: TStrings; Delimiter: Char): string;
 
 function Base64Encode(Input: WideString): string;
@@ -58,12 +60,22 @@ function Base64Decode(Input: WideString): string;
 function DrawXMLFromADO(qry: TADOQuery): string;
 function DrawXMLFromDBX(qry: TSQLQuery): string;
 
+function SetRegValue(Key, Value: string): Boolean;
+function GetRegValue(Key: string): string;
+function IsAppStartUp(app: string): Boolean;
+function SetAppStartUp(app: string): Boolean;
+function UnsetAppStartUP(app: string): Boolean;
+
 implementation
 
+const
+  _KEY_ = 'YourAppname';
+  APP_KEY = 'SOFTWARE\' + _KEY_;
+  START_KEY = 'Software\Microsoft\Windows\CurrentVersion\Run';
 
-(* *
-  *  get instance from this class
-  * *)
+  (* *
+    *  get instance from this class
+    * *)
 constructor TAssocArray.Create;
 begin
   Head.Key := '';
@@ -234,7 +246,7 @@ begin
     if toAdd = nil then
     begin
       Exit;
-        
+
     end;
   end
   else
@@ -281,28 +293,23 @@ begin
 
 end;
 
-
-
-
-
-procedure TAssocArray.fromXML(xFile: widestring);
-var 
-  Node,lNode, SubNode: IXMLDOMNode;
+procedure TAssocArray.fromXML(xFile: WideString);
+var
+  Node, lNode, SubNode: IXMLDOMNode;
   I: Integer;
 begin
   xml := CoDOMDocument.Create;
   xml.loadXML(xFile);
-//  xml.
-  Node :=  xml.firstChild;
-  lNode := Node.firstChild;  
-  for I := 0 to Node.childNodes.length-1  do
+  // xml.
+  Node := xml.firstChild;
+  lNode := Node.firstChild;
+  for I := 0 to Node.childNodes.length - 1 do
   begin
-    vSet(lNode.firstChild.text,lNode.firstChild.nextSibling.text);
+    vSet(lNode.firstChild.Text, lNode.firstChild.nextSibling.Text);
     lNode := lNode.nextSibling;
-  end;  
-  
-end;
+  end;
 
+end;
 
 (* *
   *  Get application ccurrent directory .
@@ -411,10 +418,12 @@ end;
 (* *
   *   implode strings
   * *)
+
 function Implode(Str: TStrings; Delimiter: Char): string;
 var
   I: Integer;
 begin
+
   Result := '';
   for I := 0 to Str.Count - 1 do
   begin
@@ -422,7 +431,6 @@ begin
   end;
   Delete(Result, length(Result), 1);
 end;
-
 
 (* *
   *   Addoquery to xml
@@ -436,35 +444,34 @@ var
 begin
   lst := TStringList.Create;
   qry.GetFieldNames(lst);
-  Result := '<root>'+ #13#10;
-  Result := Result + '    <cols>'+ #13#10;
+  Result := '<root>' + #13#10;
+  Result := Result + '    <cols>' + #13#10;
   for I := 0 to lst.Count - 1 do
   begin
-    Result := Result + '        <col>' + lst.Strings[I] + '</col>'+ #13#10;
+    Result := Result + '        <col>' + lst.Strings[I] + '</col>' + #13#10;
   end;
-  Result := Result + '    </cols>'+ #13#10;
+  Result := Result + '    </cols>' + #13#10;
 
-  Result := Result + '    <records>'+ #13#10;
+  Result := Result + '    <records>' + #13#10;
   for I := 0 to qry.RecordCount - 1 do
   begin
-    Result := Result + '        <record>'+ #13#10;
+    Result := Result + '        <record>' + #13#10;
     for j := 0 to lst.Count - 1 do
     begin
       Result := Result + '            <field>';
       Result := Result + Trim(qry.Fields[j].AsString);
-      Result := Result + '</field>'+ #13#10;
+      Result := Result + '</field>' + #13#10;
     end;
-    Result := Result + '        </record>'+ #13#10;
+    Result := Result + '        </record>' + #13#10;
     qry.Next;
   end;
-  Result := Result + '    </records>'+ #13#10;
-  Result := Result + '</root>'+ #13#10;
+  Result := Result + '    </records>' + #13#10;
+  Result := Result + '</root>' + #13#10;
 end;
 
 (* *
   *   TSQLQuery to xml
   * *)
-
 
 function DrawXMLFromDBX(qry: TSQLQuery): string;
 
@@ -476,43 +483,157 @@ var
 begin
   lst := TStringList.Create;
   qry.GetFieldNames(lst);
-  Result := '<root>'+ #13#10;
-  Result := Result + '    <cols>'+ #13#10;
+  Result := '<root>' + #13#10;
+  Result := Result + '    <cols>' + #13#10;
   for I := 0 to lst.Count - 1 do
   begin
-    Result := Result + '        <col>' + lst.Strings[I] + '</col>'+ #13#10;
+    Result := Result + '        <col>' + lst.Strings[I] + '</col>' + #13#10;
   end;
-  Result := Result + '    </cols>'+ #13#10;
+  Result := Result + '    </cols>' + #13#10;
 
-  Result := Result + '    <records>'+ #13#10;
+  Result := Result + '    <records>' + #13#10;
   for I := 0 to qry.RecordCount - 1 do
   begin
-    Result := Result + '        <record>'+ #13#10;
+    Result := Result + '        <record>' + #13#10;
     for j := 0 to lst.Count - 1 do
     begin
       Result := Result + '            <field>';
       Result := Result + Trim(qry.Fields[j].AsString);
-      Result := Result + '</field>'+ #13#10;
+      Result := Result + '</field>' + #13#10;
     end;
-    Result := Result + '        </record>'+ #13#10;
+    Result := Result + '        </record>' + #13#10;
     qry.Next;
   end;
-  Result := Result + '    </records>'+ #13#10;
-  Result := Result + '</root>'+ #13#10;
+  Result := Result + '    </records>' + #13#10;
+  Result := Result + '</root>' + #13#10;
 end;
-
 
 (* *
   *  show fatal error in  application
   * *)
 
-procedure FatalError(Title,Text:string;Terminate:Boolean);
+procedure FatalError(Title, Text: string; Terminate: Boolean);
 begin
-   MessageBoxW(0,PWideChar(Text),PWideChar(Title),16);
-   if (Terminate) then Application.Terminate;
+  MessageBoxW(0, PWideChar(Text), PWideChar(Title), 16);
+  if (Terminate) then
+    Application.Terminate;
 end;
 
 
+(* *
+  *  set registry key in app
+  * *)
+
+function SetRegValue(Key, Value: string): Boolean;
+var
+  rg: TRegistry;
+begin
+  rg := TRegistry.Create;
+  rg.RootKey := HKEY_CURRENT_USER;
+  if rg.OpenKey(APP_KEY, True) then
+  begin
+    rg.WriteString(Key, Value);
+    Result := True;
+  end
+  else
+  begin
+    Result := False;
+  end;
+  rg.Free;
+end;
+
+
+(* *
+  *  get resgitery keu in app
+  * *)
+
+function GetRegValue(Key: string): string;
+var
+  rg: TRegistry;
+begin
+  rg := TRegistry.Create;
+  rg.RootKey := HKEY_CURRENT_USER;
+  if rg.OpenKey(APP_KEY, False) then
+  begin
+    if rg.ValueExists(Key) then
+    begin
+      Result := rg.ReadString(Key);
+    end
+    else
+    begin
+      Result := 'null';
+    end;
+  end
+  else
+  begin
+    Result := 'null';
+  end;
+  rg.Free;
+end;
+
+(* *
+  *  is application start up function
+  * *)
+
+function IsAppStartUp(app: string): Boolean;
+var
+  rg: TRegistry;
+begin
+  rg := TRegistry.Create;
+  rg.RootKey := HKEY_CURRENT_USER;
+  Result := False;
+  if rg.OpenKey(START_KEY, False) then
+  begin
+    if ((rg.ValueExists(_KEY_)) and (rg.ReadString(_KEY_) = app)) then
+    begin
+      Result := True;
+    end;
+  end;
+  rg.Free;
+end;
+
+(* *
+  *  set application start up
+  * *)
+
+
+function SetAppStartUp(app: string): Boolean;
+var
+  rg: TRegistry;
+begin
+  rg := TRegistry.Create;
+  rg.RootKey := HKEY_CURRENT_USER;
+  Result := False;
+  if rg.OpenKey(START_KEY, True) then
+  begin
+    rg.WriteString(_KEY_, app);
+    Result := True;
+  end;
+  rg.Free;
+end;
+
+
+(* *
+  *  unset Application startup
+  * *)
+
+function UnsetAppStartUP(app: string): Boolean;
+var
+  rg: TRegistry;
+begin
+  rg := TRegistry.Create;
+  rg.RootKey := HKEY_CURRENT_USER;
+  Result := False;
+  if rg.OpenKey(START_KEY, False) then
+  begin
+    if rg.ValueExists(_KEY_) and (rg.ReadString(_KEY_) = app) then
+    begin
+      rg.DeleteValue(_KEY_);
+      Result := True;
+    end;
+  end;
+  rg.Free;
+end;
+
 
 end.
-
